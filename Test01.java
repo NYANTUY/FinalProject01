d6834aeb13f36cad814491ea2a7046c1da4e5a74
okeokke <okeokke77@gmail.com>
2025-09-28 23:39:59 +0700

    Mengubah isi Test01.java

--- Test01.java
+++ Test01.java
@@ -1,6 +1,6 @@
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("Hai. ini baris yang dimodifikasi oleh akbar");
-        System.out.println("akbar menambahkan baris ini");
+        System.out.println("Hai. ini baris yang dimodifikasi oleh Abil");
+        System.out.println("Abil menambahkan baris ini");
     }
 }
